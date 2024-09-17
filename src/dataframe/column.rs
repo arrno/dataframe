6cae6a199a870ee26f66a066169a91be32394299
Aaron Hough <aaronhough@Aarons-MBP.fios-router.home>
2024-09-17 19:40:48 -0400

    continuing to build numeric describe with quartiles. adding describe_object

--- src/dataframe/column.rs
+++ src/dataframe/column.rs
@@ -1,4 +1,6 @@
 use crate::{cell::*, dataframe::Dataframe};
+use std::cmp::Ordering;
+use std::collections::HashMap;
 
 #[derive(Debug, PartialEq)]
 pub struct Col {
@@ -51,30 +53,83 @@ impl Col {
             typed: self.typed.clone(),
         }
     }
-    // TODO -> zero div err, bubble up err, non numeric
-    pub fn describe(&self) -> Dataframe {
-        // if not numeric do: [count, unique, top, freq]
+    fn describe_object_empty(&self) -> Dataframe {
+        Dataframe::from_rows(
+            vec!["count", "unique", "top", "frequency"],
+            vec![vec![Cell::Int(0), null_float(), null_float(), null_float()]],
+        )
+        .unwrap()
+    }
+
+    pub fn describe_object(&self) -> Dataframe {
+        if self.values.len() == 0 {
+            return self.describe_object_empty();
+        }
+        let mut df = Dataframe::new(None);
+        let mut freq: HashMap<String, usize> = HashMap::new();
+        let mut most = 0;
+        let mut top = &self.typed;
+        self.values.iter().for_each(|cell| {
+            let val = freq.entry(cell.as_string()).or_insert(0);
+            *val += 1;
+            if *val > most {
+                most = *val;
+                top = cell;
+            }
+        });
+        df.add_col::<u64>("count", vec![self.values.len() as u64])
+            .unwrap();
+        df.add_cell_col("unique".to_string(), vec![Cell::Uint(freq.len() as u64)])
+            .unwrap();
+        df.add_cell_col("top".to_string(), vec![top.clone()])
+            .unwrap();
+        df.add_cell_col("frequency".to_string(), vec![Cell::Uint(most as u64)])
+            .unwrap();
+        df
+    }
+
+    fn describe_num_empty(&self) -> Dataframe {
+        Dataframe::from_rows(
+            vec!["count", "mean", "std", "min", "25%", "50%", "75%", "max"],
+            vec![vec![
+                Cell::Int(0),
+                null_float(),
+                null_float(),
+                null_float(),
+                null_float(),
+                null_float(),
+                null_float(),
+                null_float(),
+            ]],
+        )
+        .unwrap()
+    }
+    // TODO -> zero div err, bubble up err
+    pub fn describe_numeric(&self) -> Dataframe {
+        if self.values.len() == 0 {
+            return self.describe_num_empty();
+        }
         let mut df = Dataframe::new(None);
         df.add_col::<u64>("count", vec![self.values.len() as u64])
             .unwrap();
-        let mut min = match self.values.get(0) {
-            Some(cell) => cell,
-            None => &self.typed().zero(),
-        };
-        let mut max = min;
-        let mut total = min.clone();
-        self.values.iter().for_each(|cell| {
-            if cell < min {
-                min = cell
-            }
-            if cell > max {
-                max = cell
-            }
-            if let Some(c) = total.add(cell) {
-                total = c
-            }
+        let mut total = self.typed.zero();
+        let mut sorted_set: Vec<Cell> = self
+            .values
+            .iter()
+            .map(|cell| {
+                if let Some(c) = total.add(cell) {
+                    total = c;
+                }
+                cell.clone()
+            })
+            .collect();
+        sorted_set.sort_by(|cur, prev| match cur > prev {
+            true => Ordering::Greater,
+            false => Ordering::Less,
         });
-        let mean = total.div_float(self.values.len() as f64).unwrap();
+        let min = &sorted_set[0];
+        let max = &sorted_set[sorted_set.len() - 1];
+        let mean = total.div_float(sorted_set.len() as f64).unwrap();
         let mean_f = match mean {
             Cell::Float(m) => m,
             _ => 0.0,
@@ -98,12 +153,67 @@ impl Col {
             .unwrap();
         df.add_cell_col("min".to_string(), vec![min.clone()])
             .unwrap();
-        // 25%
-        // 50%
-        // 75%
+        match quartiles(&sorted_set) {
+            Some((quart, med, sev_fifth)) => {
+                df.add_cell_col("25%".to_string(), vec![quart]).unwrap();
+                df.add_cell_col("50%".to_string(), vec![med]).unwrap();
+                df.add_cell_col("75%".to_string(), vec![sev_fifth]).unwrap();
+            }
+            None => {
+                df.add_cell_col("25%".to_string(), vec![null_float()])
+                    .unwrap();
+                df.add_cell_col("50%".to_string(), vec![null_float()])
+                    .unwrap();
+                df.add_cell_col("75%".to_string(), vec![null_float()])
+                    .unwrap();
+            }
+        };
         df.add_cell_col("max".to_string(), vec![max.clone()])
             .unwrap();
         df
+    }
+}
+
+fn null_float() -> Cell {
+    Cell::Null(Box::new(Cell::Float(0.0)))
+}
+
+fn quartiles(sorted_set: &Vec<Cell>) -> Option<(Cell, Cell, Cell)> {
+    match sorted_set.len() {
+        0 => None,
+        1 => Some((
+            sorted_set[0].clone(),
+            sorted_set[0].clone(),
+            sorted_set[0].clone(),
+        )),
+        _ => {
+            let med_idx = sorted_set.len() as f64 / 2.0;
+            let med = match med_idx.floor() {
+                med_idx => sorted_set
+                    .get(med_idx as usize)?
+                    .add(sorted_set.get(1 + med_idx as usize)?)?
+                    .div_float(2.0)?,
+                _ => sorted_set.get(med_idx.ceil() as usize)?.clone(),
+            };
+            let quart_idx = sorted_set.len() as f64 / 4.0;
+            let (quart, sev_fifth) = match quart_idx.floor() {
+                quart_idx => (
+                    sorted_set
+                        .get(quart_idx as usize)?
+                        .add(sorted_set.get(1 + quart_idx as usize)?)?
+                        .div_float(2.0)?,
+                    sorted_set
+                        .get((quart_idx * 3.0) as usize)?
+                        .add(sorted_set.get(1 + (quart_idx * 3.0) as usize)?)?
+                        .div_float(2.0)?,
+                ),
+                _ => (
+                    sorted_set.get(quart_idx.ceil() as usize)?.clone(),
+                    sorted_set.get((quart_idx * 3.0).ceil() as usize)?.clone(),
+                ),
+            };
+            Some((quart, med, sev_fifth))
+        }
     }
 }
 
